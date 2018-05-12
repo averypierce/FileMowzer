d020e1edbbc70edcdf8df1fed4343018b4849f25
Avery Pierce VanKirk <averypierce@users.noreply.github.com>
2018-05-12 13:43:47 -0700

    changed to a bootstrap navbar

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import Main from "./Main";
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 ReactDOM.render(
   <Main/>, 
